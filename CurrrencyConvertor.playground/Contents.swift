59eed3013ea27a510536ac1ecefa24acd9c6d8ce
Mark Poggi <markp@mac.com>
2020-03-12 14:27:37 -0700

    v1 Currency Convertor Project

---
+++ CurrrencyConvertor.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+enum Currency: String {
+    case cad
+    case mxn
+    case usd
+}
+
+let usToCad = 1.39
+let usToMxn = 22.34
+
+var currency: Currency = .cad
+
+func convert(dollars: Double, targetCurrency: Currency) -> Double {
+    switch currency {
+    case .cad:
+        return dollars * usToCad
+    case .mxn:
+        return dollars * usToMxn
+    case .usd:
+        return dollars
+    }
+}
+
+convert(dollars: 15, targetCurrency: .cad)
+
+//6. Create a function called `convert(amountString: String) -> String?` In it, do the following:
+//    * create a constant called `amount`. Its value should be the `amountString` that is initialized into a `Double`
+//    * use a `guard let` to unwrap the new `amount` constant.
+//    * convert the dollar amount to the expected currency (hint, you'll want to call the `convert` method you created in step 4
+    
+func convert2(amountString: String) -> String? {
+    guard let amount = Double(amountString) else {
+        return nil
+    }
+        let convertedAmount = convert(dollars: amount, targetCurrency: .cad)
+        return String(convertedAmount)
+    }
+
+convert2(amountString: "12")
+    
+//7. Test your code by calling `convert(amountString` with some values. Change your `currency` to both `.cad` and `.mxn` (by manually changing the value of your `currency` variable above) to make sure the conversion works for both currencies.

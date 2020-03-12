31733983150b584233518769869aa43482798ed0
Violet Lavender Love <violetlove@Junyper.local>
2020-03-12 15:41:11 -0700

    currencyConverter update

--- currencyConverter.playground/Contents.swift
+++ currencyConverter.playground/Contents.swift
@@ -4,3 +4,25 @@ enum Currency {
     case mxn
 }
 let usdToCad: Double = 1.39
+let usdToMxn: Double = 21.71
+var currency = Currency.cad
+func convert(_ dollars: Double) -> Double {
+    switch currency {
+    case .cad:
+     return dollars * usdToCad
+    case .mxn:
+     return dollars * usdToMxn
+    }
+}
+func convert(amountString: String) -> String?{
+    guard let amount = Double(amountString) else{
+        return "empty string"
+    }
+    let dollar = convert(amount)
+    print(String(dollar))
+    return String(dollar)
+}
+convert(amountString: "27356.57")
+currency = .mxn
+convert(amountString: "165987.65")
+

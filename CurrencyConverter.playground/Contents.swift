d44e02d86797e2ad44fa533f5368e0478dcd2c74
Bronson Mullens <bronsonmullens@icloud.com>
2020-03-18 22:37:27 -0700

    Updated code to include optional portion

--- CurrencyConverter.playground/Contents.swift
+++ CurrencyConverter.playground/Contents.swift
@@ -3,5 +3,43 @@ import Cocoa
 enum Currency {
     case cad
     case mxn
-    
 }
+
+//Conversion rates as of 3/18/20 10:03 PM PDT according to Morningstar
+let usToCad: Double = 1.46
+let usToMxn: Double = 24.32
+
+class Converter {
+    var currency: Currency
+    
+    init(currency: Currency = .cad) {
+        self.currency = currency
+    }
+    
+    func convert(_ dollars: Double) -> Double {
+        switch currency {
+        case .cad:
+            return(dollars * usToCad)
+        case .mxn:
+            return(dollars * usToMxn)
+        }
+    }
+}
+
+let newCurrency = Converter()
+
+//Converts USD to CAN/MXN and prints the result as a String in currency format
+func convert(amountString: String) -> String {
+    guard let amount = Double(amountString),
+        amount > 0 else { return "" }
+    let currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }()
+    print(currencyFormatter.string(from: NSNumber(value: newCurrency.convert(amount)))!)
+    newCurrency.convert(amount)
+    return amountString
+}
+
+var finalConversion = convert(amountString: "20")

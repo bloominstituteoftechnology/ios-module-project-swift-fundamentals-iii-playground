1676640bb42ecfe70d078973b5bf4f4368e51024
roverare <roverare@gmail.com>
2020-04-09 19:32:31 -0400

    Completed Project

--- CurrencyConverter.playground/Contents.swift
+++ CurrencyConverter.playground/Contents.swift
@@ -3,32 +3,32 @@ import UIKit
 enum Currency {
     case cad
     case mxn
-    
-    
-    }
- 
-    
+}
+
+
+
+var currencyFormatter: NumberFormatter = {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .currency
+    return formatter
+}()
+
 
 let usToCad = 1.40
 let usToMxn = 23.63
 
 
-var currency:Currency = .cad
+var currency:Currency = .mxn
 
 
 func convert(_ dollars: Double) -> Double {
-
+    
     if currency == .cad {
-        
         return usToCad * dollars
-        
     } else if  currency == .mxn {
-        
         return usToMxn * dollars
-   
     } else {
-        
-        return dollars
+         return dollars
     }
 }
 
@@ -36,13 +36,12 @@ func convert(amountString: String) -> String? {
     
     
     guard let amount = Double(amountString) else{return nil}
-        
-    let newAmount = convert(amount)
     
-    return String(newAmount)
-
+    let convertedAmount = convert(amount)
+    
+    return currencyFormatter.string(from: NSNumber(value: convertedAmount))
+    
 }
-
 
 convert(amountString: "12")
 

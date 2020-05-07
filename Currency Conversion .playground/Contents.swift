62e5542047070e797f0495418dc5edbca27c7088
Ian Becker <ian-becker@lambdastudents.com>
2020-05-07 13:08:47 -0700

    Created a Currency Enum, Conversion constants, and a converssion function

--- Currency Conversion .playground/Contents.swift
+++ Currency Conversion .playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var str = "Hello, playground"
+// $1 USD = $1.40 CAD
+// $1 USD = $24.07 MXN
+
+enum Currency {
+    case cad
+    case mxn
+}
+
+let usToCad = 1 / 1.40
+let usToMxn = 1 / 24.07
+
+var currency: Currency = .cad
+
+func convert(_ dollars: Double) -> Double {
+    if currency == .cad {
+        return dollars / 1.40
+    } else {
+        return dollars / 24.07
+    }
+}
+
+convert(20)
+

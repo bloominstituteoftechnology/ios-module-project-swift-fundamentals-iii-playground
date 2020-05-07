cd61a28e7ee8eedb0d7c9a6d4ef0b41a03a40f32
swiftalicious <amazinggrace1@protonmail.com>
2020-05-07 12:21:20 -0700

    Created a currency type enum

---
+++ CurrencyConverter.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+enum Currency {
+    case cad
+    case mxn
+}
+
+let usToCad = 1.40
+let usToMxn = 23.59
+
+var currency: Currency = .cad
+
+let usd: Double = 1.00
+
+func convert(dollars: Double) -> Double {
+    switch currency {
+    case .cad:
+        return usToCad
+    case .mxn:
+        return usToMxn
+    default:
+        return dollars
+    }
+}
+
+func convert(amountString: String) -> String? {
+//    let amount: Double
+//    amount = amountString
+//
+//   guard let amount = amountString else {
+//    print("Invalid amount")
+//    return
+//    }
+//}
+//var currencyFormatter = NumberFormatter = {
+//  let formatter = NumberFormatter()
+//    formatter.numberStyle = .currency
+//    return formatter
+//} ()
+
+
+func multiply(firstNumber: Int, secondNumber: Int) -> Int {
+   let total = firstNumber * secondNumber
+    return total
+}
+
+let numberOne = 4
+let numberTwo = 5
+
+multiply(firstNumber: numberOne, secondNumber: numberTwo)

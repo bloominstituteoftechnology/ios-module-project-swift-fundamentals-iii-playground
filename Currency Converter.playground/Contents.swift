427bba65fc4434a9d041ffe647de31dbc48a5da5
Richard Gibbs <richgibbs@Richs-MacBook.attlocal.net>
2020-05-07 16:47:14 -0700

    added two more functions to format currency based on region currency came from

--- Currency Converter.playground/Contents.swift
+++ Currency Converter.playground/Contents.swift
@@ -21,17 +21,32 @@ var currency: Currency? = .cad
    func convert(dollars: Double) -> Double {
     if currency == .cad {
        let newAmount = dollars * usToCad
-        print(newAmount)
+        print("\(convertDoubleToCurrency2(amount: newAmount))")
     } else if currency == .mxn  {
         let otherAmount = dollars * usToMxn
-            print(otherAmount)
+            print("\(convertDoubleToCurrency(amount: otherAmount))")
         }
     return dollars
 }
 
-
+func convertDoubleToCurrency(amount: Double) -> String {
+      let numberFormatter = NumberFormatter()
+      numberFormatter.numberStyle = .currency
+      numberFormatter.locale = Locale(identifier: "es_MX")
+      
+      return numberFormatter.string(from: NSNumber(value: amount))!
+  }
     
+func convertDoubleToCurrency2(amount: Double) -> String {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .currency
+    numberFormatter.locale = Locale(identifier: "fr_CA")
+    
+    return numberFormatter.string(from: NSNumber(value: amount))!
+}
 
 convert(dollars: 12)
 convert(dollars: 309)
 convert(dollars: 6470)
+
+

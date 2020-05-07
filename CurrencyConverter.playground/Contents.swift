f2b08238f88c46439727011913fc7bf7ad839c1a
violet-love <violetlove9419@icloud.com>
2020-05-07 13:00:26 -0700

    created a convert function with a switch statement inside to check if its mxn or cad

--- CurrencyConverter.playground/Contents.swift
+++ CurrencyConverter.playground/Contents.swift
@@ -12,5 +12,20 @@ let usdToMXN = 24.08
 
 var currency: Currency = .cad
 
-
+func convert(_ dollars: Double) -> Double {
+    
+    switch currency {
+        
+    case .cad:
+        let canadianDollars = dollars * usdToCAD
+        return canadianDollars
+        
+    case .mxn:
+        let pesos = dollars * usdToMXN
+        return pesos
+        
+    default:
+        print("Hi, developer here. You shouldn't be here, but if you are, please take a moment to remember all the developers hard at work right now keeping our society alive. Please remember that the next time you consider torrenting an app rather than paying 99 cents. Have a great day! <3")
+    }
+}
 

153297dffb98bd9911f9d279de3e03889b0209bc
Richard Gibbs <richgibbs@Richs-MacBook.attlocal.net>
2020-05-07 12:19:22 -0700

    Not sure if this is working

--- Currency Converter.playground/Contents.swift
+++ Currency Converter.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+// Currency Conversion
+
+// $1 USD to $1.40 CAD
+// $1 USD to $24.06 MXN
+
+enum Currency {
+    case cad
+    case mxn
+
+}
+
+let usToCad = 1.40
+let 

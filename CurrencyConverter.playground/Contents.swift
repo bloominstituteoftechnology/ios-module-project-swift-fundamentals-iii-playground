d3f2b1ccce54e50bc3177d5a931d263a281e27c5
James McDougall <jameswiliammcdougall@gmail.com>
2020-06-29 14:32:20 -0500

    Project Setup

---
+++ CurrencyConverter.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+enum Currency {
+    case cad
+    case mxn
+}
+
+let usToCad: Double = 1.33
+let usToMxn: Double = 19.70
+
+var currency: Currency = .cad
+
+func convert(_ dollars: Double) -> Double {
+    
+    return dollars
+}

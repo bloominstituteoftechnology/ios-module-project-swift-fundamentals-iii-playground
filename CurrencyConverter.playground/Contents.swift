a79284685293dcabe5842d304400b594e1f05053
James McDougall <jameswiliammcdougall@gmail.com>
2020-07-01 23:17:37 -0500

    Added convert function

--- CurrencyConverter.playground/Contents.swift
+++ CurrencyConverter.playground/Contents.swift
@@ -11,6 +11,11 @@ let usToMxn: Double = 19.70
 var currency: Currency = .cad
 
 func convert(_ dollars: Double) -> Double {
-    
-    return dollars
+    if currency == .cad {
+        let value = dollars * usToCad
+        return value
+    } else {
+        let value = dollars * usToMxn
+        return value
+    }
 }

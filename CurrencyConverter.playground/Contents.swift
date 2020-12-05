a1e78267fb2b4acb421ba9b1138ebb248d52e30b
James McDougall <jameswilliammcdougall@gmail.com>
2020-12-05 13:21:32 -0600

    Step 4 logic complete

--- CurrencyConverter.playground/Contents.swift
+++ CurrencyConverter.playground/Contents.swift
@@ -11,5 +11,13 @@ let usToMxn: Double = 19.70
 var currency: Currency = .cad
 
 func convert(_ dollars: Double) -> Double {
-    
+    if currency == .cad {
+        let dollars = usToCad * 1
+        return dollars
+        
+    } else if currency == .mxn {
+        let dollars = usToMxn * 1
+        return dollars
+    }
+    return dollars
 }

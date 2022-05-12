1396f05683c5750e4921df703114b01927891791
Waseem Idelbi <idelbiw@gmail.com>
2022-05-11 20:44:22 -0400

    I got the number formatter working, but it's returning the result of the conversion looking like USD when this is supposed to be converted FROM USD. I'm going to make another function that customizes the number formatter to make the result look appropriate and match its own currency type.

--- README.playground/Contents.swift
+++ README.playground/Contents.swift
@@ -9,6 +9,12 @@ let usToCad = 1.30
 let usToMxn = 20.32
 var currency: Currency = .cad
 
+var currencyFormatter: NumberFormatter = {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .currency
+    return formatter
+}()
+
 func convert(_ dollars: Double) -> Double {
     var result = 0.0
     switch currency {
@@ -37,4 +43,8 @@ currency = .mxn
 print(convert(100))
 if let successfulConversion = convert(amountString: "100") {
     print(successfulConversion)
+}
+
+if let successfulFormatting = currencyFormatter.string(from: convert(100) as NSNumber) {
+    print(successfulFormatting)
 }
